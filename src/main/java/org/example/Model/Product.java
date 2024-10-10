9a04b551a2cb18c522cf45d3bb9a764434fc05e9
Vasile <vasile.ceban1@isa.utm.md>
2024-10-10 17:50:48 +0300

    create product model

---
+++ src/main/java/org/example/Model/Product.java
@@ -0,0 +1,60 @@
+package org.example.Model;
+
+public class Product {
+
+    String name;
+    Double price;
+    String color;
+    String currency;
+    String time;
+    String link;
+
+    public Product() {}
+
+    public Product(String name, Double price, String currency, String link) {
+        this.name = name;
+        this.price = price;
+        this.currency = currency;
+        this.link = link;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getLink() {
+        return link;
+    }
+}
