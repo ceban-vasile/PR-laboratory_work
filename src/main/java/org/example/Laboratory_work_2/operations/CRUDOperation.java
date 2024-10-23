660e07447bad2dcc07d9e07218cceba3e8f61847
Vasile <vasile.ceban1@isa.utm.md>
2024-10-23 20:09:25 +0300

    added insertProductToDB method to insert the data in database with statement

--- src/main/java/org/example/Laboratory_work_2/operations/CRUDOperation.java
+++ src/main/java/org/example/Laboratory_work_2/operations/CRUDOperation.java
@@ -1,5 +1,47 @@
 package org.example.Laboratory_work_2.operations;
 
-public class CRUDOperation {
+import org.example.Laboratory_work_2.interface_connect_db.Connect_DB;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class CRUDOperation implements Connect_DB {
+    public CRUDOperation(){}
+
+    private void insertProductToDB(String name, String color, double price, String currency, String time, String link) throws SQLException {
+
+        //Connect to database
+        Connection connect = connect();
+
+        //create query to insert a product
+        String sql = "INSERT INTO products(name, color, price, currency, time, link) VALUES (?, ?, ?, ?, ?, ?)";
+
+        PreparedStatement preparedStatement = connect.prepareStatement(sql);
+
+        preparedStatement.setString(1, name);
+        preparedStatement.setString(2, color);
+        preparedStatement.setDouble(3, price);
+        preparedStatement.setString(4, currency);
+        preparedStatement.setString(5, time);
+        preparedStatement.setString(6, link);
+
+        int rowsInserted = preparedStatement.executeUpdate();
+        System.out.println(rowsInserted + " row(s) inserted.");
+
+        preparedStatement.close();
+        connect.close();
+    }
+
+    @Override
+    public Connection connect() {
+        Connection conn = null;
+        try {
+            DriverManager.getConnection("jdbc:postgresql://localhost:5433/data_product", "postgres", "admin1234");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return conn;
+    }
 }
