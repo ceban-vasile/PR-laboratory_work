011c8c50fb3e525344ef374ba1aa27868674fb11
Vasile <vasile.ceban1@isa.utm.md>
2024-10-23 23:16:02 +0300

    added updateProductToDB method to update the price according the name

--- src/main/java/org/example/Laboratory_work_2/operations/CRUDOperation.java
+++ src/main/java/org/example/Laboratory_work_2/operations/CRUDOperation.java
@@ -10,13 +10,13 @@ import java.sql.SQLException;
 public class CRUDOperation implements Connect_DB {
     public CRUDOperation(){}
 
-    private void insertProductToDB(String name, String color, double price, String currency, String time, String link) throws SQLException {
+    public void insertProductToDB(String name, String color, double price, String currency, String time, String link) throws SQLException {
 
         //Connect to database
         Connection connect = connect();
 
         //create query to insert a product
-        String sql = "INSERT INTO products(name, color, price, currency, time, link) VALUES (?, ?, ?, ?, ?, ?)";
+        String sql = "INSERT INTO products(name, color, price, currency, time_convert, link) VALUES (?, ?, ?, ?, ?, ?)";
 
         PreparedStatement preparedStatement = connect.prepareStatement(sql);
 
@@ -34,13 +34,34 @@ public class CRUDOperation implements Connect_DB {
         connect.close();
     }
 
+    public void updateProductToDB(String name, double price) throws SQLException {
+
+        Connection connect = connect();
+
+        String sql = "UPDATE products SET price=? WHERE name=?";
+        PreparedStatement preparedStatement = connect.prepareStatement(sql);
+
+        preparedStatement.setString(2, name);
+        preparedStatement.setDouble(1, price);
+
+        int rowsUpdated = preparedStatement.executeUpdate();
+        System.out.println(rowsUpdated + " row(s) updated.");
+
+        connect.close();
+        preparedStatement.close();
+    }
+
     @Override
     public Connection connect() {
         Connection conn = null;
         try {
-            DriverManager.getConnection("jdbc:postgresql://localhost:5433/data_product", "postgres", "admin1234");
+            conn = DriverManager.getConnection(
+                    "jdbc:postgresql://localhost:5433/data_product",
+                    "postgres",
+                    "admin1234"
+            );
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException("Failed to connect to the database", e);
         }
         return conn;
     }
