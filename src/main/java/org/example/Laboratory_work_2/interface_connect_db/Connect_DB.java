de3983d423daade7b1f7ba808350b2fd6d256fd3
Vasile <vasile.ceban1@isa.utm.md>
2024-10-24 13:52:13 +0300

    deleted the Connect_DB.java class

--- src/main/java/org/example/Laboratory_work_2/interface_connect_db/Connect_DB.java
+++
@@ -1,9 +0,0 @@
-package org.example.Laboratory_work_2.interface_connect_db;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-
-public interface Connect_DB {
-    Connection connect();
-}
