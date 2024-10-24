d81e5c0bed91db6c6d6412e248dcbc75148fa5d1
Vasile <vasile.ceban1@isa.utm.md>
2024-10-24 14:08:11 +0300

    added GET and UPDATE handler

--- src/main/java/org/example/Laboratory_work_2/server/HTTPHandler.java
+++ src/main/java/org/example/Laboratory_work_2/server/HTTPHandler.java
@@ -1,7 +1,9 @@
 package org.example.Laboratory_work_2.server;
 
+import com.google.gson.Gson;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
+import org.example.Laboratory_work_1.Model.Product;
 import org.example.Laboratory_work_2.operations.CRUDOperation;
 import org.json.JSONObject;
 
@@ -9,12 +11,12 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.nio.charset.StandardCharsets;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class HTTPHandler implements HttpHandler {
 
     public HTTPHandler() {}
-
-    CRUDOperation addProduct = new CRUDOperation();
 
     @Override
     public void handle(HttpExchange exchange) throws IOException {
@@ -32,7 +34,7 @@ public class HTTPHandler implements HttpHandler {
             String link = json.getString("link");
 
             try {
-                addProduct.insertProductToDB(name, color, price, currency, time_converted, link);
+                new CRUDOperation().insertProductToDB(name, color, price, currency, time_converted, link);
             } catch (SQLException e) {
                 throw new RuntimeException(e);
             }
@@ -51,13 +53,49 @@ public class HTTPHandler implements HttpHandler {
             String name = json.getString("name");
 
             try {
-                addProduct.deleteProductToDB(name);
+                new CRUDOperation().deleteProductToDB(name);
             } catch (SQLException e) {
                 throw new RuntimeException(e);
             }
 
             String response = "Product deleted successfully!";
             exchange.sendResponseHeaders(200, response.length());
+            OutputStream os = exchange.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }else if("UPDATE".equals(exchange.getRequestMethod())){
+
+            byte[] body = exchange.getRequestBody().readAllBytes();
+            String requestBody = new String(body, StandardCharsets.UTF_8);
+
+            JSONObject json = new JSONObject(requestBody);
+            String name = json.getString("name");
+            double price = json.getDouble("price");
+
+            try {
+                new CRUDOperation().updateProductToDB(name, price);
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+
+            String response = "Product update successfully!";
+            exchange.sendResponseHeaders(200, response.length());
+            OutputStream os = exchange.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }else if("GET".equals(exchange.getRequestMethod())){
+
+            String response;
+            try {
+                List<Product> products = new CRUDOperation().displayProductsToDB();
+                Gson gson = new Gson();
+                response = gson.toJson(products);
+            } catch (SQLException e) {
+                e.printStackTrace();
+                response = "Database error: " + e.getMessage();
+            }
+
+            exchange.sendResponseHeaders(200, response.getBytes().length);
             OutputStream os = exchange.getResponseBody();
             os.write(response.getBytes());
             os.close();
